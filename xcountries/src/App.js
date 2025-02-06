eacfa893c9cf5e3db40d1d9a973031b58f48a438
Dheeraj Kumar <dk8549644@gmail.com>
2025-02-06 22:45:55 +0530

    Add initial project structure with basic components and configuration

---
+++ xcountries/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import CountryFlags from './CountryFlags';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='heading'>XCountries </h1>
+      <CountryFlags />
+    </div>
+  );
+}
+
+export default App;
---
+++ xcountries/src/CountryFlags.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+const CountryCard = ({name, flag}) => {
+    return (
+        <div className="country-card">
+            <img src={flag} alt={`Flag of ${name}`} className="flag-img" />
+            <h3>{name}</h3>
+        </div>
+    );
+}
+
+
+function CountryFlags() {
+    const API_ENDPOINT = "https://xcountries-backend.azurewebsites.net/all";
+    const [flagData, setFlagData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await fetch(API_ENDPOINT);
+                const jsonData = await data.json();
+                setFlagData(jsonData);
+            }
+            catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div className="country-flag-container">
+            {flagData.map((country) => (
+                <CountryCard key={country.abbr} name={country.name} flag={country.flag} />
+            ))}
+        </div>
+    );
+}
+
+
+export default CountryFlags;
